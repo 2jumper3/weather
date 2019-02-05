adcf12cbe5e4a31a9587e31441d0f82f2e2f3bf2
2jumper3 <2jumper3@gmail.com>
2019-02-05 15:25:25 +0300

    update

---
+++ WeatherApp/Models/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  WeatherApp
+//
+//  Created by Sergey on 04/02/2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
